8fe5ac5564e34c9667c688643fcb228c17d42356
cfop0329-oss <cfop0329@gmail.com>
2026-05-02 11:03:52 +0500

    Tsis

---
+++ TSIS/TSIS1/connect.py
@@ -0,0 +1,9 @@
+import psycopg2
+from config import host, dbname, user, password
+def connect():
+    return psycopg2.connect(
+        host=host,
+        dbname=dbname,
+        user=user,
+        password=password
+    )
---
+++ TSIS/TSIS4/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "snake_db",
+    "user": "postgres",
+    "password": "Kpostre18",
+    "port": 5432
+}
